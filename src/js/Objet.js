7f349dedf98cc53783c47d76adb0011182216fdc
johannadlcb <delachambre.johanna@gmail.com>
2019-12-08 00:12:17 +0100

    test pour ajouter des textures

--- src/js/Objet.js
+++ src/js/Objet.js
@@ -11,6 +11,31 @@ class Objet{
         ctx.fillRect(this.x,this.y,this.taille,this.taille);
         ctx.restore();
     }
+
+    // --------- TEST POUR DESSINER LES TEXTURES -------------
+    // --------- Pas d'erreur dans la console, mais ne fonctionne pas -------------
+    /*draw(){
+        ctx.save();
+        ctx.fillStyle = this.couleur;
+        if (this.couleur == "#0C5210"){
+            ctx.fillRect(this.x,this.y,this.taille,this.taille);
+            var img = new Image();   // Crée un nouvel élément img
+            img.addEventListener('load', function() {
+                
+                ctx.drawImage(img, this.x, this.y);
+            }, false);
+            
+            img.src = this.image; // définit le chemin de la source
+            
+        }
+        else {
+            ctx.fillRect(this.x,this.y,this.taille,this.taille); 
+        }
+        ctx.restore();
+        
+    }
+
+    */
     collisionObjetHup(hup){
         if (hup.x == this.x && hup.y == this.y && this.etat == true){
             if (hup.vie < 3 && this.type == "coeur"){
--- src/js/classesDecor/arbre.js
+++ src/js/classesDecor/arbre.js
@@ -1,3 +1,4 @@
 class arbre extends Objet{
     couleur = "#0C5210";
+    //image = "js/classesDecor/tree.jpg";  TEST TEXTURES
   }
