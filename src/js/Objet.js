415830c17c9b836859ead17e6a272fa9dbcfb0fb
johannadlcb <delachambre.johanna@gmail.com>
2019-12-08 17:49:10 +0100

    ajout d'images pour les arbres, les herbes et l'eau

--- src/js/Objet.js
+++ src/js/Objet.js
@@ -5,37 +5,53 @@ class Objet{
         this.taille = 32;
         this.etat = true;
         }
+    /* POUR DESSINER SANS TEXTURES 
+    
     draw(){
         ctx.save();
         ctx.fillStyle = this.couleur;
         ctx.fillRect(this.x,this.y,this.taille,this.taille);
         ctx.restore();
-    }
+    }*/
 
-    // --------- TEST POUR DESSINER LES TEXTURES -------------
-    // --------- Pas d'erreur dans la console, mais ne fonctionne pas -------------
-    /*draw(){
+    // --------- POUR DESSINER AVEC TEXTURES -------------
+    draw(){
         ctx.save();
-        ctx.fillStyle = this.couleur;
+        ctx.fillStyle = "#D2A75C";
         if (this.couleur == "#0C5210"){
+            var image = new Image();
+            image.src = 'js/classesDecor/tree.png';
             ctx.fillRect(this.x,this.y,this.taille,this.taille);
-            var img = new Image();   // Crée un nouvel élément img
-            img.addEventListener('load', function() {
-                
-                ctx.drawImage(img, this.x, this.y);
-            }, false);
+            ctx.drawImage(image, this.x, this.y,32,32);
             
-            img.src = this.image; // définit le chemin de la source
+            
+        }
+        else if (this.couleur == "#348D12"){
+            var image = new Image();
+            image.src = 'js/classesDecor/herbe.png';
+            ctx.fillRect(this.x,this.y,this.taille,this.taille);
+            ctx.drawImage(image, this.x, this.y,32,32);
+            
+            
+        }
+        else if (this.couleur == "#3570A4" || this.couleur == "#0b2045"){
+            ctx.fillStyle = this.couleur;
+            var image = new Image();
+            image.src = 'js/classesDecor/water.png';
+            ctx.fillRect(this.x,this.y,this.taille,this.taille);
+            ctx.drawImage(image, this.x, this.y,32,32);
+            
             
         }
         else {
+            ctx.fillStyle = this.couleur;
             ctx.fillRect(this.x,this.y,this.taille,this.taille); 
         }
         ctx.restore();
         
     }
 
-    */
+    
     collisionObjetHup(hup){
         if (hup.x == this.x && hup.y == this.y && this.etat == true){
             if (hup.vie < 3 && this.type == "coeur"){
--- src/js/classesDecor/arbre.js
+++ src/js/classesDecor/arbre.js
@@ -1,4 +1,5 @@
 class arbre extends Objet{
     couleur = "#0C5210";
     //image = "js/classesDecor/tree.jpg";  TEST TEXTURES
+    
   }
